488d36db7a189e3eafe13bef3ddf8e1365e1dc3d
aleksandr_sa <coldsun93@mail.ru>
2023-02-14 22:27:59 +0300

    add scanner for num and oper

--- Homework_java_1/hw_robot.java
+++ Homework_java_1/hw_robot.java
@@ -1,5 +1,29 @@
 package Homework_java_1;
 
+import java.util.Scanner;
+
 public class hw_robot {
-    
+
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int numA = getNumberByUser("Введите числа а (а - натурально, целое число) ");
+        int numB = getNumberByUser("Введите числа b (b - натурально, целое число, b>a) ");
+        String operC = getStrByUser("Введите операцию d (при операции d, число a -> b) ");
+        String operD = getStrByUser("Введите операцию c (d>c) ");
+
+        // System.out.println(number + "-ый элемент треугольного числа = " +
+        // Decision(number) + "\n");
+    }
+
+    public static int getNumberByUser(String text) {
+        System.out.print(text);
+        return input.nextInt();
+    }
+
+    public static String getStrByUser(String text) {
+        System.out.print(text);
+        return input.nextLine();
+    }
+
 }
