15c5d285b80df118c85aa90a55281c7c0957a19e
aleksandr_sa <coldsun93@mail.ru>
2023-02-23 15:17:30 +0300

    change file readme, add description of the fourth homework

--- Homework_java_3/hw_sl.java
+++ Homework_java_3/hw_sl.java
@@ -3,46 +3,46 @@ package Homework_java_3;
 public class hw_sl {
 
     public static void main(String[] args) {
-        int[] actual = { 7, 2, 1, 6, 3, 4, 8, 5, 7 };
-        mergeSort(actual, actual.length);
-        printArr(actual);
+        int[] arrayIn = { 7, 2, 1, 6, 3, 4, 8, 5, 7 };
+        mergeSort(arrayIn, arrayIn.length);
+        printArr(arrayIn);
     }
 
-    private static void mergeSort(int[] a, int n) {
+    private static void mergeSort(int[] arr, int n) {
         if (n < 2) {
             return;
         }
         int mid = n / 2;
-        int[] l = new int[mid];
-        int[] r = new int[n - mid];
+        int[] leftArr = new int[mid];
+        int[] rightArr = new int[n - mid];
 
         for (int i = 0; i < mid; i++) {
-            l[i] = a[i];
+            leftArr[i] = arr[i];
         }
         for (int i = mid; i < n; i++) {
-            r[i - mid] = a[i];
+            rightArr[i - mid] = arr[i];
         }
-        mergeSort(l, mid);
-        mergeSort(r, n - mid);
+        mergeSort(leftArr, mid);
+        mergeSort(rightArr, n - mid);
 
-        merge(a, l, r, mid, n - mid);
+        merge(arr, leftArr, rightArr, mid, n - mid);
     }
 
-    private static void merge(int[] a, int[] l, int[] r, int left, int right) {
+    private static void merge(int[] arr, int[] leftArr, int[] rightArr, int left, int right) {
 
         int i = 0, j = 0, k = 0;
         while (i < left && j < right) {
-            if (l[i] <= r[j]) {
-                a[k++] = l[i++];
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k++] = leftArr[i++];
             } else {
-                a[k++] = r[j++];
+                arr[k++] = rightArr[j++];
             }
         }
         while (i < left) {
-            a[k++] = l[i++];
+            arr[k++] = leftArr[i++];
         }
         while (j < right) {
-            a[k++] = r[j++];
+            arr[k++] = rightArr[j++];
         }
     }
 
