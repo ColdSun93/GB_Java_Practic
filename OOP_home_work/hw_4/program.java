e739383d8c68b36623afdee6658949bf6b3d1146
aleksandr_sa <coldsun93@mail.ru>
2023-03-20 19:59:29 +0300

    change implementation of tasks from 1 to 9 in 'OOP_home_work\hw_4'

--- OOP_home_work/hw_4/program.java
+++ OOP_home_work/hw_4/program.java
@@ -4,6 +4,24 @@ public class program {
 
     public static void main(String[] args) {
         ArrayDinamic<Integer> newList = new ArrayDinamic<>();
+        newList.addElement(10);
+        newList.addElement(15);
+        newList.addElement(12);
+        newList.addElement(16);
+        newList.addElement(9);
+        newList.addElement(22);
+        newList.addElement(22);
+        newList.addElement(22);
+        newList.printArr();
+        newList.removeIndexData(2);
+        newList.removeByValueArr(22);
+        newList.searchMin();
+        newList.searchMax();
+        newList.sumElemArr();
+        newList.multiElemArr();
+        newList.searchByIndex(7);
+        newList.checkInElem(77);
+
     }
     
 }
