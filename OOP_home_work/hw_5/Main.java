e97a8cec94655363b4dfcc01bd873140625eed33
aleksandr_sa <coldsun93@mail.ru>
2023-04-01 18:28:24 +0300

    add logger in Calculator

--- OOP_home_work/hw_5/Main.java
+++ OOP_home_work/hw_5/Main.java
@@ -1,14 +1,24 @@
 package OOP_home_work.hw_5;
 
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
-        
-
-        ViewMenu.menuUser(input);
+        LoggerOperation log = new LoggerOperation();
+        log.initLogger();
+        try {
+            ViewMenu.menuUser(input, log);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         input.close();
+      
+
+
     }
 }
--- OOP_home_work/hw_5/ViewMenu.java
+++ OOP_home_work/hw_5/ViewMenu.java
@@ -1,34 +1,37 @@
 package OOP_home_work.hw_5;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class ViewMenu {
 
-    static void menuUser(Scanner input) {
+    static void menuUser(Scanner input, LoggerOperation log) throws IOException {
         System.out.println("==========Выберете тип чисел=============");
         System.out.println("1. рациональные");
         System.out.println("2. комплексные");
         System.out.println("3. рациональные");
 
-        int key = input.nextInt();
+        int key = input.nextInt();     
         System.out.print("Введите первое число:\n");
         double firstNumber = input.nextDouble();
+        log.info(Double.toString(firstNumber));
         System.out.print("Введите второе число:\n");
         double secondNumber = input.nextDouble();
+        log.info(Double.toString(secondNumber));
 
 
         switch (key) {
             case 1:
                 RationalNum rationalNum = new RationalNum(firstNumber, secondNumber);
-                menuRationalNum(input, rationalNum);
+                menuRationalNum(input, rationalNum, log);
                 break;
             case 2:
                 ComplexNum complexNum = new ComplexNum(firstNumber, secondNumber);
-                menuComplexNum(input, complexNum);
+                menuComplexNum(input, complexNum, log);
                 break;
             case 3:
                 BinaryNum binaryNum = new BinaryNum();
-                menuBinaryNum(input, binaryNum);
+                menuBinaryNum(input, binaryNum, log);
                 break;
             default:
                 break;
@@ -36,9 +39,10 @@ public class ViewMenu {
      
     }
 
-    private static void menuBinaryNum(Scanner input, BinaryNum binaryNum) {
+    private static void menuBinaryNum(Scanner input, BinaryNum binaryNum, LoggerOperation log) {
         PrintOperation(); 
         char operation = input.next().charAt(0);
+        log.info(String.valueOf(operation));
         String result;
         switch (operation) {
             case '+':
@@ -59,12 +63,15 @@ public class ViewMenu {
                 System.out.printf("Please enter specified operator only");
                 return;
         }
+        System.out.println("результат " + result);
     }
 
-    private static void menuComplexNum(Scanner input, ComplexNum complexNum) {
+    private static void menuComplexNum(Scanner input, ComplexNum complexNum, LoggerOperation log) {
         PrintOperation(); 
         String result;
         char operation = input.next().charAt(0);
+        log.info(String.valueOf(operation));
+
         switch (operation) {
             case '+':
             result = complexNum.sumElement();
@@ -84,6 +91,7 @@ public class ViewMenu {
                 System.out.printf("Please enter specified operator only");
                 return;
         }
+        System.out.println("результат " + result);
     }
 
     static void PrintOperation() {
@@ -96,10 +104,11 @@ public class ViewMenu {
 
     }
 
-    static void menuRationalNum (Scanner input, RationalNum rationalNum) {
+    static void menuRationalNum (Scanner input, RationalNum rationalNum, LoggerOperation log) {
         PrintOperation(); 
         String result;
         char operation = input.next().charAt(0);
+        log.info(String.valueOf(operation));
         switch (operation) {
             case '+':
             result = rationalNum.sumElement();
@@ -120,6 +129,7 @@ public class ViewMenu {
                 return;
         }
         System.out.println("результат " + result);
+        log.info(result);
 
     }
 
